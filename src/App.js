b905f8526d057ca9f00a5091a94f0f879cd49df1
Eric Yon <ericyon24@gmail.com>
2023-11-12 11:55:46 -0600

    more styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-
+import { ReactSVG } from "react-svg";
 function App() {
   const [user, setUser] = useState({
     firstName: "",
@@ -12,7 +12,27 @@ function App() {
   const paragraph =
     "See how experienced developers solve problems in real time. Watching scripted tutorials is great, but understanding how developers think is invaluable.";
 
-  const USER_INFO = ["First Name", "Last Name", "Email", "Password"];
+  const handleOnChange = (e) => {
+    switch (e.target.id) {
+      case "firstName":
+        setUser({ ...user, firstName: e.target.value });
+        break;
+      case "lastName":
+        setUser({ ...user, lastName: e.target.value });
+        break;
+      case "email":
+        setUser({ ...user, email: e.target.value });
+        break;
+      case "password":
+        setUser({ ...user, password: e.target.value });
+        break;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
 
   return (
     <main className="App">
@@ -26,12 +46,62 @@ function App() {
             <span>Try it free 7 days then </span>$20/mo. thereafter
           </span>
         </div>
-        <form action="none" className="user__form">
-          <input type="text" placeholder="First Name" value={user.firstName} />
-          <input type="text" placeholder="Last Name" value={user.lastName} />
-          <input type="text" placeholder="Email" value={user.email} />
-          <input type="text" placeholder="Password" value={user.password} />
-          <button className="user__form--button">CLAIM YOUR FREE TRIAL</button>
+        <form onSubmit={handleSubmit} action="none" className="user__form">
+          <input
+            type="text"
+            id="firstName"
+            placeholder="First Name"
+            value={user.firstName}
+            onChange={handleOnChange}
+          />
+          <div className="user__form--error">
+            <ReactSVG src="icon-error.svg" className="user__form--errorIcon" />
+            <span className="user__form--errorMessage">
+              First Name cannot be empty
+            </span>
+          </div>
+          <input
+            type="text"
+            id="lastName"
+            placeholder="Last Name"
+            value={user.lastName}
+            onChange={handleOnChange}
+          />
+          <div className="user__form--error">
+            <ReactSVG src="icon-error.svg" className="user__form--errorIcon" />
+            <span className="user__form--errorMessage">
+              Last Name cannot be empty
+            </span>
+          </div>
+          <input
+            type="text"
+            id="email"
+            placeholder="Email"
+            value={user.email}
+            onChange={handleOnChange}
+          />
+          <div className="user__form--error">
+            <ReactSVG src="icon-error.svg" className="user__form--errorIcon" />
+            <span className="user__form--errorMessage">
+              Looks like this is not an email
+            </span>
+          </div>
+          <input
+            type="text"
+            id="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleOnChange}
+          />
+          <div className="user__form--error">
+            <ReactSVG src="icon-error.svg" className="user__form--errorIcon" />
+            <span className="user__form--errorMessage">
+              Password cannot be empty
+            </span>
+          </div>
+          <button className="user__form--button" type="submit">
+            CLAIM YOUR FREE TRIAL
+          </button>
           <span className="user__form--terms">
             By clicking the button, you are agreeing to our{" "}
             <a>Terms and Services</a>
